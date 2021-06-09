84c6a54c1d9013136e2ace828181feea28820d0d
Zaeem Tarrar <zaeemtarrar3@gmail.com>
2021-06-09 19:51:54 +0500

    nodejs 0.0.2

--- fundamentals/server-1/src/app.ts
+++ fundamentals/server-1/src/app.ts
@@ -1,135 +1,154 @@
-let AppStarted = Date.now();
-import { red, green, blue, cyan, magenta, yellow, bold, gray } from 'colors';
-import express from 'express';
-import session from 'express-session';
+let AppStarted = Date.now()
+import { red, green, blue, cyan, magenta, yellow, bold, gray } from 'colors'
+import express from 'express'
+import session from 'express-session'
 // import logger from 'express-logger'
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import redis from 'redis';
-import http from 'http';
-import morgan from 'morgan';
-import { NOT_FOUND, INTERNAL_SERVER_ERROR, OK } from 'http-status';
-const logger = morgan('combined');
-const { sequelize } = require('./database/sequelize');
-const { PORT, IP, CreateDb, RefreshDb, SeedDb, RemoveDb } = require('./configurations/index');
-const R = require('./utils/response');
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import redis from 'redis'
+import http from 'http'
+import morgan from 'morgan'
+import { NOT_FOUND, INTERNAL_SERVER_ERROR, OK } from 'http-status'
+const logger = morgan('combined')
+const { sequelize } = require('./database/sequelize')
+const {
+  PORT,
+  IP,
+  CreateDb,
+  RefreshDb,
+  SeedDb,
+  RemoveDb,
+} = require('./configurations/index')
+const R = require('./utils/response')
 // const { fire } = require('./fire')
 
-const app = express();
+const app = express()
 
 /**
  * Relationships
  */
-const BuildModelRelations = require('./db-helpers/relationship');
+const BuildModelRelations = require('./db-helpers/relationship')
 
 /**
  * Db Table Dropper
  */
-const DbDropper = require('./db-helpers/drop');
+const DbDropper = require('./db-helpers/drop')
 
 /**
  * Seeder Methods
  */
-const { Seeder } = require('./seeder/index');
+const { Seeder } = require('./seeder/index')
 
 /**
  * Middleware Imports
  */
 // const auth = require('./middlewares/auth/index');
-const mainGate = require('./middlewares/mainGate/index');
+const mainGate = require('./middlewares/mainGate/index')
 
 /**
  * Router Imports
  */
-const NatureRouter = require('./routes/nature');
+const NatureRouter = require('./routes/nature')
 
 // app.use(logger)
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(
-	session({
-		secret: '123456',
-		saveUninitialized: false,
-		resave: false
-	})
-);
-app.use(cors());
+  session({
+    secret: '123456',
+    saveUninitialized: false,
+    resave: false,
+  }),
+)
+app.use(cors())
 
 /**
  * Middleware Integration
  */
-app.use(mainGate);
+app.use(mainGate)
 
 /**
  * Main Routes
  */
-app.use('/nature', NatureRouter);
+app.use('/nature', NatureRouter)
 
 /**
  * Regular Routes
  */
 app.use('/test', (req, res, next) => {
-	res.json(R(OK, 'Application is Running Perfectly'));
-});
+  res.json(R(OK, 'Application is Running Perfectly'))
+})
 
 app.use('/', (req, res, next) => {
-	res.json(R(NOT_FOUND, null));
-});
+  res.json(R(NOT_FOUND, null))
+})
 
 app.use((req, res, next) => {
-	res.json(R(INTERNAL_SERVER_ERROR, null));
-});
-
-const Chain = new Promise<void>((resolve, reject) => resolve());
-Chain.then((): void => {
-	console.clear();
+  res.json(R(INTERNAL_SERVER_ERROR, null))
 })
-	.then((): void => {
-		if (RemoveDb) {
-			console.log(red(`\nRemoving All Tables from Database`));
-			// return sequelize.drop();
-			return DbDropper();
-		}
-	})
-	.then((): void => {
-		// Relation Definitions
-		return BuildModelRelations();
-	})
-	.then((): void => {
-		// Database Creation / Refreshing / Seeding
-		if (CreateDb) {
-			return sequelize
-				.sync({ force: RefreshDb })
-				.then((result: any) => {
-					console.log(blue(`\nSQL Database Connected`));
-					if (RefreshDb) {
-						console.log(blue(`SQL Database Refreshed`));
-					}
-				})
-				.then((): any => {
-					if (SeedDb) {
-						return Seeder();
-					}
-				})
-				.catch((err: Error): void => {
-					console.log(red('Sequelize Error: '), gray(JSON.stringify(err.message, null, 2)));
-				});
-		} else {
-			return;
-		}
-	})
-	.then((): void => {
-		// Main Server
-		const server: http.Server = http.createServer(app);
-		server.listen(PORT, (): void => {
-			let AppServerStarted = Date.now();
-			let TimeTaken = (AppServerStarted - AppStarted) / 1000.0;
-			console.log(
-				bold(green(`\nApplication Started at ${red(IP)}:${blue(PORT)}`)),
-				bold(gray(`\t ( Server Execution Time: ${green(TimeTaken.toString() + ` seconds`)} )`))
-			);
-		});
-	})
-	.catch((err: Error) => {
-		console.log(bold(red(`Server Error: `)), bold(gray(JSON.stringify(err, null, 2))));
-	});
+
+const Chain = new Promise<void>((resolve, reject) => resolve())
+Chain.then((): void => {
+  console.clear()
+})
+  .then((): void => {
+    if (RemoveDb) {
+      console.log(red(`\nRemoving All Tables from Database`))
+      // return sequelize.drop();
+      return DbDropper()
+    }
+  })
+  .then((): void => {
+    // Relation Definitions
+    return BuildModelRelations()
+  })
+  .then((): void => {
+    // Database Creation / Refreshing / Seeding
+    if (CreateDb) {
+      return sequelize
+        .sync({ force: RefreshDb })
+        .then((result: any) => {
+          console.log(blue(`\nSQL Database Connected`))
+          if (RefreshDb) {
+            console.log(blue(`SQL Database Refreshed`))
+          }
+        })
+        .then((): any => {
+          if (SeedDb) {
+            return Seeder()
+          }
+        })
+        .catch((err: Error): void => {
+          console.log(
+            red('Sequelize Error: '),
+            gray(JSON.stringify(err.message, null, 2)),
+          )
+        })
+    } else {
+      return
+    }
+  })
+  .then((): void => {
+    // Main Server
+    const server: http.Server = http.createServer(app)
+    server.listen(PORT, (): void => {
+      let AppServerStarted = Date.now()
+      let TimeTaken = (AppServerStarted - AppStarted) / 1000.0
+      console.log(
+        bold(green(`\nApplication Started at ${red(IP)}:${blue(PORT)}`)),
+        bold(
+          gray(
+            `\t ( Server Execution Time: ${green(
+              TimeTaken.toString() + ` seconds`,
+            )} )`,
+          ),
+        ),
+      )
+    })
+  })
+  .catch((err: Error) => {
+    console.log(
+      bold(red(`Server Error: `)),
+      bold(gray(JSON.stringify(err, null, 2))),
+    )
+  })
--- fundamentals/server-1/src/seeder/index.ts
+++ fundamentals/server-1/src/seeder/index.ts
@@ -1,147 +1,168 @@
-import { cyan, red, green, blue, yellow, bold, gray } from 'colors';
-const { GenerateHash } = require('./../utils/password');
+import { cyan, red, green, blue, yellow, bold, gray } from 'colors'
+const { GenerateHash } = require('./../utils/password')
 
 /**
  * Type Contract Imports
  */
-import { T_Nature } from '../contracts/model/nature';
-import { T_User } from '../contracts/model/user';
-import { T_Profile } from '../contracts/model/profile';
-import { T_Product } from '../contracts/model/product';
-import { T_Category } from '../contracts/model/category';
-import { T_Tag } from '../contracts/model/tag';
+import { T_Nature } from '../contracts/model/nature'
+import { T_User } from '../contracts/model/user'
+import { T_Profile } from '../contracts/model/profile'
+import { T_Product } from '../contracts/model/product'
+import { T_Category } from '../contracts/model/category'
+import { T_Tag } from '../contracts/model/tag'
 
 /**
  * Db Model Imports
  */
-const Nature = require('./../models/nature');
-const User = require('./../models/user');
-const Profile = require('./../models/profile');
-const Product = require('./../models/product');
-const Category = require('./../models/category');
-const Tag = require('./../models/tag');
+const Nature = require('./../models/nature')
+const User = require('./../models/user')
+const Profile = require('./../models/profile')
+const Product = require('./../models/product')
+const Category = require('./../models/category')
+const Tag = require('./../models/tag')
 
 module.exports.Seeder = (): Promise<void> | void => {
-	try {
-		console.log(bold(cyan(`\nInitializing Database Seeder`)));
-		/**
+  try {
+    console.log(bold(cyan(`\nInitializing Database Seeder`)))
+    /**
      * Dummy Data Loadings
      */
-		let natures: T_Nature[] = require('./../data/seeds/nature.json').data;
-		let users: T_User[] = require('./../data/seeds/user.json').data;
-		let profiles: T_Profile[] = require('./../data/seeds/profile.json').data;
-		let categories: T_Category[] = require('./../data/seeds/category.json').data;
-		let tags: T_Tag[] = require('./../data/seeds/tag.json').data;
-		let products: T_Product[] = require('./../data/seeds/product.json').data;
+    let natures: T_Nature[] = require('./../data/seeds/nature.json').data
+    let users: T_User[] = require('./../data/seeds/user.json').data
+    let profiles: T_Profile[] = require('./../data/seeds/profile.json').data
+    let categories: T_Category[] = require('./../data/seeds/category.json').data
+    let tags: T_Tag[] = require('./../data/seeds/tag.json').data
+    let products: T_Product[] = require('./../data/seeds/product.json').data
 
-		/**
+    /**
      * Hashing Password Promise Method
      */
-		const users_password_promises = users.map((row) => {
-			return GenerateHash(row.password);
-		});
-		const users_hash_passwords: Promise<void> = Promise.all([ ...users_password_promises ]).then((result: any) => {
-			for (let i in users) {
-				users[i].password = result[i];
-			}
-		});
+    const users_password_promises = users.map((row) => {
+      return GenerateHash(row.password)
+    })
+    const users_hash_passwords: Promise<void> = Promise.all([
+      ...users_password_promises,
+    ]).then((result: any) => {
+      for (let i in users) {
+        users[i].password = result[i]
+      }
+    })
 
-		/**
+    /**
      * Pivot Table Seeding Method
      */
-		const Pivot = (
-			Table1: object | any,
-			Table2: object | any,
-			Table2String: string,
-			id1: number,
-			id2: number
-		): Promise<void> | void => {
-			try {
-				return Table1.findOne({ where: { id: id1 } }).then((tb1: any) => {
-					Table2.findOne({ where: { id: id2 } }).then((tb2: any) => {
-						tb1[`add${Table2String}`](tb2);
-					});
-				});
-			} catch (err) {
-				console.log(bold(red(`Pivot Seeding Error: (${Table1}: ${id1}) (${Table2}: ${id2})`)));
-			}
-		};
+    const Pivot = (
+      Table1: object | any,
+      Table2: object | any,
+      Table2String: string,
+      id1: number,
+      id2: number,
+    ): Promise<void> | void => {
+      try {
+        return Table1.findOne({ where: { id: id1 } }).then((tb1: any) => {
+          Table2.findOne({ where: { id: id2 } }).then((tb2: any) => {
+            tb1[`add${Table2String}`](tb2)
+          })
+        })
+      } catch (err) {
+        console.log(
+          bold(
+            red(`Pivot Seeding Error: (${Table1}: ${id1}) (${Table2}: ${id2})`),
+          ),
+        )
+      }
+    }
 
-		/**
+    /**
      * Hashing User Passwords before Seeding
      */
-		return users_hash_passwords
-			.then(() => {
-				/**
-        * Seed Promises
-        */
-				let Natures_Promise: Promise<void> = Nature.bulkCreate(natures).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`Nature`)} Table Seeded`)));
-					return result;
-				});
-				let Users_Promise: Promise<void> = User.bulkCreate(users).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`User`)} Table Seeded`)));
-					return result;
-				});
-				let Profiles_Promise: Promise<void> = Profile.bulkCreate(profiles).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`Profile`)} Table Seeded`)));
-					return result;
-				});
-				let Categories_Promise: Promise<void> = Category.bulkCreate(categories).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`Category`)} Table Seeded`)));
-					return result;
-				});
-				let Tags_Promise: Promise<void> = Tag.bulkCreate(tags).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`Tag`)} Table Seeded`)));
-					return result;
-				});
-				let Products_Promise: Promise<void> = Product.bulkCreate(products).then((result: any): any => {
-					console.log(bold(cyan(`${blue(`Product`)} Table Seeded`)));
-					return result;
-				});
+    return users_hash_passwords
+      .then(() => {
+        /**
+         * Seed Promises
+         */
+        let Natures_Promise: Promise<void> = Nature.bulkCreate(natures).then(
+          (result: any): any => {
+            console.log(bold(cyan(`${blue(`Nature`)} Table Seeded`)))
+            return result
+          },
+        )
+        let Users_Promise: Promise<void> = User.bulkCreate(users).then(
+          (result: any): any => {
+            console.log(bold(cyan(`${blue(`User`)} Table Seeded`)))
+            return result
+          },
+        )
+        let Profiles_Promise: Promise<void> = Profile.bulkCreate(profiles).then(
+          (result: any): any => {
+            console.log(bold(cyan(`${blue(`Profile`)} Table Seeded`)))
+            return result
+          },
+        )
+        let Categories_Promise: Promise<void> = Category.bulkCreate(
+          categories,
+        ).then((result: any): any => {
+          console.log(bold(cyan(`${blue(`Category`)} Table Seeded`)))
+          return result
+        })
+        let Tags_Promise: Promise<void> = Tag.bulkCreate(tags).then(
+          (result: any): any => {
+            console.log(bold(cyan(`${blue(`Tag`)} Table Seeded`)))
+            return result
+          },
+        )
+        let Products_Promise: Promise<void> = Product.bulkCreate(products).then(
+          (result: any): any => {
+            console.log(bold(cyan(`${blue(`Product`)} Table Seeded`)))
+            return result
+          },
+        )
 
-				/**
-        * Main Seeding Chain
-        */
-				const SeedChain: Promise<any[]> = Promise.all([
-					/**
-          * Model Seeding Promises
-          */
-					Natures_Promise,
-					Users_Promise,
-					Profiles_Promise,
-					Categories_Promise,
-					Tags_Promise,
-					Products_Promise
-				]);
-				return SeedChain.then((values: any) => {
-					return values.map((value: any, index: any): void => {
-						// Do Something
-					});
-				})
-					.then((): void => {
-						/**
-           * Pivot Table Seeding Section
+        /**
+         * Main Seeding Chain
+         */
+        const SeedChain: Promise<any[]> = Promise.all([
+          /**
+           * Model Seeding Promises
            */
-						let Fn = () => {
-							Pivot(Tag, Product, 'Product', 1, 1);
-							Pivot(Tag, Product, 'Product', 3, 1);
-							Pivot(Tag, Product, 'Product', 2, 2);
-							Pivot(Tag, Product, 'Product', 4, 2);
-						};
-						return Fn();
-					})
-					.then((): void => {
-						console.log(bold(cyan(`Database Tables Seeded Successfully`)));
-					})
-					.catch((err: Error): void => {
-						console.log(red('Database Tables Seeding Error: '), gray(err.message));
-					});
-			})
-			.catch((e: Error) => {
-				console.log(bold(red(`Error: ${e.message || null}`)));
-			});
-	} catch (err) {
-		console.log(bold(red(`Error: ${err}`)));
-	}
-};
+          Natures_Promise,
+          Users_Promise,
+          Profiles_Promise,
+          Categories_Promise,
+          Tags_Promise,
+          Products_Promise,
+        ])
+        return SeedChain.then((values: any) => {
+          return values.map((value: any, index: any): void => {
+            // Do Something
+          })
+        })
+          .then((): void => {
+            /**
+             * Pivot Table Seeding Section
+             */
+            let Fn = () => {
+              Pivot(Tag, Product, 'Product', 1, 1)
+              Pivot(Tag, Product, 'Product', 3, 1)
+              Pivot(Tag, Product, 'Product', 2, 2)
+              Pivot(Tag, Product, 'Product', 4, 2)
+            }
+            return Fn()
+          })
+          .then((): void => {
+            console.log(bold(cyan(`Database Tables Seeded Successfully`)))
+          })
+          .catch((err: Error): void => {
+            console.log(
+              red('Database Tables Seeding Error: '),
+              gray(err.message),
+            )
+          })
+      })
+      .catch((e: Error) => {
+        console.log(bold(red(`Error: ${e.message || null}`)))
+      })
+  } catch (err) {
+    console.log(bold(red(`Error: ${err}`)))
+  }
+}
